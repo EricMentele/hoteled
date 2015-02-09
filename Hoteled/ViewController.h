182be8d0b09b1cd698d3a54394246f306bbe638d
Eric Mentele <diviningsage@gmail.com>
2015-02-09 09:30:17 -0800

    Initial Commit

---
+++ Hoteled/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hoteled
+//
+//  Created by Eric Mentele on 2/9/15.
+//  Copyright (c) 2015 Eric Mentele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
